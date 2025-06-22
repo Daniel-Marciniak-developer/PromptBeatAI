323d34a209a44aa19cc6e560fef8825a313a4d8c
ptaqqqq <61513585+ptaqqqq@users.noreply.github.com>
2025-06-22 15:04:21 +0200

    Add sample-based piano implementation for loopmaker
    Closes #10

---
+++ promptbeatai/loopmaker/loopmaker/piano.py
@@ -0,0 +1,23 @@
+from promptbeatai.loopmaker.loopmaker.core import Note, SoundGenerator
+from pathlib import Path
+from pydub import AudioSegment
+from typing import cast, Dict
+
+
+class Piano(SoundGenerator):
+    def __init__(self, folderpath: Path):
+        super().__init__()
+        self.folderpath = folderpath
+        self.samples: Dict[Note, AudioSegment] = {}
+
+        for sample_file in self.folderpath.iterdir():
+            if sample_file.is_file():
+                note_name = sample_file.stem
+                self.samples[Note.from_name(note_name)] = AudioSegment.from_file(sample_file)
+        
+    def generate(self, note: Note, duration_ms: int):
+        # TODO: AHDSR envelope
+        if note not in self.samples:
+            raise ValueError(f'No sample found for note {note.name}')
+        sample = self.samples[note]
+        return cast(AudioSegment, sample[:duration_ms])
