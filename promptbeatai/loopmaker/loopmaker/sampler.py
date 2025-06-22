cb850a305c5ce6040c1c6cb78122e7d896ab4551
ptaqqqq <61513585+ptaqqqq@users.noreply.github.com>
2025-06-22 12:42:32 +0200

    Add Sampler as an implementation of SoundGenerator
    Closes #7

---
+++ promptbeatai/loopmaker/loopmaker/sampler.py
@@ -0,0 +1,16 @@
+from promptbeatai.loopmaker.loopmaker.core import Note, SoundGenerator
+from typing import cast
+from pathlib import Path
+from pydub import AudioSegment
+
+
+class Sampler(SoundGenerator):
+    def __init__(self, filepath: Path):
+        super().__init__()
+        self.filepath = filepath
+        self.sound: AudioSegment = AudioSegment.from_file(self.filepath)
+
+    def generate(self, note: Note, duration: int) -> AudioSegment:
+        # PyDub doesn't have type annotations, this is a workaround
+        return cast(AudioSegment, self.sound[:duration])
+
