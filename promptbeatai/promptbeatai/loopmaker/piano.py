bd55ceca7641fe634d4076030e18fe9f3c94a658
Daniel Marciniak <danielmarciniak.business@gmail.com>
2025-07-01 13:51:29 +0200

    merging branch development with main
    * Add pitch shifting algorithm to use in loopmaker.piano
    
    Previous behavior was to raise an exception when a particular sample was not available.
    
    Closes #17
    
    * add vitae #23
    
    * add .vite folder to gitignore
    
    ---------
    
    Co-authored-by: ptaqqqq <61513585+ptaqqqq@users.noreply.github.com>

--- promptbeatai/promptbeatai/loopmaker/piano.py
+++ promptbeatai/promptbeatai/loopmaker/piano.py
@@ -13,11 +13,39 @@ class Piano(SoundGenerator):
         for sample_file in self.folderpath.iterdir():
             if sample_file.is_file():
                 note_name = sample_file.stem
-                self.samples[Note.from_name(note_name)] = AudioSegment.from_file(sample_file)
+                note = Note.from_name(note_name)
+                self.samples[note] = AudioSegment.from_file(sample_file)
+
+        if not self.samples:
+            raise ValueError('No available samples to play')
+
+    def _pitch_shift(self, requested_note: Note):
+        # Find closest note
+        min_dist = 1_000_000
+        closest_note = None
+        for avail_note in self.samples:
+            dist = abs(requested_note.midi - avail_note.midi)
+            if dist < min_dist:
+                closest_note = avail_note
+                min_dist = dist
+        if closest_note is None:
+            raise ValueError('No available sample to play!')
+        # Pitch shift the closest sample to the requested note
+        semitone_diff = requested_note.midi - closest_note.midi
+        sample = self.samples[closest_note]
+        octaves = semitone_diff / 12.0
+        new_sample = sample._spawn(sample.raw_data, overrides={
+            "frame_rate": int(sample.frame_rate * (2.0 ** octaves))
+        }).set_frame_rate(sample.frame_rate)
+        # TODO might cause issues in multithreaded context
+        self.samples[requested_note] = new_sample
         
     def generate(self, note: Note, duration_ms: int):
-        # TODO: AHDSR envelope
+        # TODO smooth clip-off if it ever becomes a problem
         if note not in self.samples:
-            raise ValueError(f'No sample found for note {note.name}')
+            # Pitch shifting, yay!
+            # OLD: raise ValueError(f'No sample found for note {note.name}')
+            self._pitch_shift(note)
+
         sample = self.samples[note]
         return cast(AudioSegment, sample[:duration_ms])
