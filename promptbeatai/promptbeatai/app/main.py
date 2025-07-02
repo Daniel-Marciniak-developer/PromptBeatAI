d9f4fe8b67c56f5c32ef5c07cb73382e4ea81862
ptaqqqq <61513585+ptaqqqq@users.noreply.github.com>
2025-07-02 16:06:29 +0200

    Add main.py in promptbeatai.app

---
+++ promptbeatai/promptbeatai/app/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+from .routers import generate_song
+
+
+app = FastAPI(
+    title='PromptBeatAI API',
+    description='Backend API for PromptBeatAI',
+)
+
+origins_env = os.getenv('ALLOWED_ORIGINS', None)
+if origins_env:
+    origins = [origin.strip() for origin in origins_env.split(',')]
+else:
+    origins = [
+        'http://localhost',
+        'http://localhost:3000',
+        'http://127.0.0.1'
+        'http://127.0.0.1:3000'
+    ]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+app.include_router(generate_song)
