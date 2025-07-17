1c4b79d83f7ddb1ce93c901cc17116e477ae6194
ptaqqqq <61513585+ptaqqqq@users.noreply.github.com>
2025-07-17 15:48:12 +0200

    Dockerize

--- frontend/project/vite.config.ts
+++ frontend/project/vite.config.ts
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_BASE_URL,
+        changeOrigin: true,
+      },
+    },
   },
 });
